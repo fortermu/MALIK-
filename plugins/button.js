f1475ea793f63d3e7280253e03a12b7ed4601984
fortermu <167023239+fortermu@users.noreply.github.com>
2024-07-24 08:10:54 +0500

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -6,13 +6,13 @@ bot({ pattern: 'button', fromMe: true, desc: 'send a button message', usage: '#b
   button: [
    { type: 'list', params: { title: 'Button 1', sections: [{ title: 'Button 1', rows: [{ header: 'title', title: 'Button 1', description: 'Description 1', id: '#menu' }] }] } },
    { type: 'reply', params: { display_text: 'MENU', id: '#menu' } },
-   { type: 'url', params: { display_text: 'Neeraj-x0', url: 'https://www.neerajx0.xyz/', merchant_url: 'https://www.neerajx0.xyz/' } },
+   { type: 'url', params: { display_text: 'MALIK-MD official WhatsApp Group', url: 'https://chat.whatsapp.com/KrJhpZhgKxZLfHcyUZKOMw', YOUTUBE_url: 'https://youtube.com/@problem_solved.?si=Rzo_P_9Ac5vSJohF' } },
    { type: 'address', params: { display_text: 'Address', id: 'message' } },
    { type: 'location', params: {} },
-   { type: 'copy', params: { display_text: 'copy', id: '123456789', copy_code: 'message' } },
-   { type: 'call', params: { display_text: 'Call', phone_number: '123456789' } },
+   { type: 'copy', params: { display_text: 'copy', id: '923263429027', copy_code: 'message' } },
+   { type: 'call', params: { display_text: 'Call', phone_number: '923263429027' } },
   ],
-  header: { title: 'X-Asena', subtitle: 'WhatsApp Bot', hasMediaAttachment: false },
+  header: { title: 'MALIK-MD', subtitle: 'WhatsApp Bot', hasMediaAttachment: false },
   footer: { text: 'Interactive Native Flow Message' },
   body: { text: 'Interactive Message' },
  }
